8159648acba3fde7f8fecefec7386baec31a56c2
antoine <antoine.doeraene@b12-consulting.com>
2020-12-12 17:16:18 +0100

    examples-doc-test for path segment

---
+++ shared/src/test/scala/urldsl/examples/package.scala
@@ -0,0 +1,14 @@
+package urldsl
+
+/**
+  * This package shows examples of usage of url-dsl.
+  *
+  * When using url-dsl, you are free to chose any error adt system that you want. There are two
+  * built-in for you, the [[urldsl.errors.SimplePathMatchingError]] (and siblings) and the
+  * [[urldsl.errors.DummyError]]. We will use the former in these example. The latter being perfect
+  * in cases where you don't care about what caused the error (for example a router) or when you use primarily
+  * url-dsl for generating urls, and not for parsing them.
+  */
+package object examples {
+  val sampleUrl = "http://www.some-domain.be/foo/23/true?bar=stuff&babar=other%20stuff#the-ref"
+}
