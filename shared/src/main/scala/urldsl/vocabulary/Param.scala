d35324ec590619914209722479bd6765a190bb12
i10416 <ito.yo16uh90616@gmail.com>
2021-12-31 20:10:20 +0900

    fix parsing empty query string
    - bug: parsing empty query parameter like `http://localhost:8080?foo=`as String  returned None instead of Some("").
    - improve: check if query parameter's key is valid. `http://localhost:8080?=value` was parsed into "" -> Param(value).

--- shared/src/main/scala/urldsl/vocabulary/Param.scala
+++ shared/src/main/scala/urldsl/vocabulary/Param.scala
@@ -14,7 +14,8 @@ object Param {
       .map(_.split("=").toList)
       .toList
       .collect {
-        case first :: second :: Nil => first -> second
+        case first :: Nil if first.nonEmpty => first -> ""
+        case first :: second :: Nil if first.nonEmpty => first -> second
       }
       .groupBy(_._1)
       .map { case (key, value) => key -> value.map(_._2) }
