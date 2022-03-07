817cc8d70595f55d50ce149c9f9f2252abf2c6ee
antoine <antoine.doeraene@b12-consulting.com>
2022-03-07 15:56:21 +0100

    FromString cleaning

--- shared/src/main/scala/urldsl/vocabulary/FromString.scala
+++ shared/src/main/scala/urldsl/vocabulary/FromString.scala
@@ -18,29 +18,23 @@ object FromString extends FromStringWithNumeric {
 
   def factory[T, A](read: String => Either[A, T]): FromString[T, A] = (str: String) => read(str)
 
+  /**
+    * Creates an instance of [[FromString]] from a function that "casts" a [[java.lang.String]] to an instance of T.
+    * This cast can be unsafe. If it fails (throws), it will be caught and wrapped in the
+    * [[ErrorFromThrowable.fromThrowable()]] method.
+    */
+  case class FromUnsafeMap[T, A](cast: String => T, fromThrowable: ErrorFromThrowable[A]) extends FromString[T, A] {
+    def fromString(str: String): Either[A, T] = Try(cast(str)).toEither.left.map(fromThrowable.fromThrowable)
+  }
+
   implicit def stringFromString[A]: FromString[String, A] = factory(Right(_))
-  implicit def intFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[Int, A] = factory(
-    s =>
-      Try(s.toInt) match {
-        case Success(int)       => Right(int)
-        case Failure(exception) => Left(fromThrowable.fromThrowable(exception))
-      }
-  )
-  implicit def doubleFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[Double, A] = factory(
-    s =>
-      Try(s.toDouble) match {
-        case Success(double)    => Right(double)
-        case Failure(exception) => Left(fromThrowable.fromThrowable(exception))
-      }
-  )
-  implicit def booleanFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[Boolean, A] = factory(
-    s =>
-      Try(s.toBoolean) match {
-        case Success(bool) => Right(bool)
-        case Failure(_)    => Left(fromThrowable.fromThrowable(new Exception(s"$s is not a Boolean")))
-      }
-  )
+  implicit def intFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[Int, A] =
+    FromUnsafeMap(_.toInt, fromThrowable)
+  implicit def doubleFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[Double, A] =
+    FromUnsafeMap(_.toDouble, fromThrowable)
+  implicit def booleanFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[Boolean, A] =
+    FromUnsafeMap(_.toBoolean, fromThrowable)
   implicit def uuidFromString[A](implicit fromThrowable: ErrorFromThrowable[A]): FromString[java.util.UUID, A] =
-    factory(s => Try(java.util.UUID.fromString(s)).toEither.left.map(fromThrowable.fromThrowable))
+    FromUnsafeMap(java.util.UUID.fromString, fromThrowable)
 
 }
